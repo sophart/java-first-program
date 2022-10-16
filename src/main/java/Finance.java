490e7e4df021b62cd825501d182ec6c873759a93
Sophart <phunsophart@gmail.com>
2022-10-16 09:39:20 +0700

    Fix Finance App

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class Finance {
-    public final static String BEST_LOAN_RATES = "bestLoanRate";
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
